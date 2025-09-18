e16f6eca71e98b14d78086ebd61e06ffe97cb17e
김동환 <donghwan@gimdonghwan-ui-MacBookAir.local>
2025-09-18 09:53:16 +0900

    merge code

---
+++ merge_hwan/merge_sex.py
@@ -0,0 +1,22 @@
+import pandas as pd
+
+# csv 파일 읽기
+weekday = pd.read_csv('평일_여가시간/하루+여가시간(평일)_성별.csv')
+holiday = pd.read_csv('휴일_여가시간/하루+여가시간(휴일)_성별.csv')
+
+# 구분 컬럼 추가
+weekday['구분'] = '평일'
+holiday['구분'] = '휴일'
+
+# 컬럼 순서 맞추기
+cols = ['성별', '구분'] + [c for c in weekday.columns if c not in ['성별', '구분']]
+weekday = weekday[cols]
+holiday = holiday[cols]
+
+# 파일 병합
+merged = pd.concat([weekday, holiday], ignore_index=True) # 기존 인덱스 무시하고 새로 부여
+
+# 병합한 파일 저장
+merged.to_csv('여가시간_평일_휴일_합본/sex.csv', index=False, encoding='utf-8-sig') # DataFrame의 인덱스를 컬럼으로 저장하지 않음
+
+print('sex.csv 저장완료')
