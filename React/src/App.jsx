5dd5db5d29c812954e3fad8fbb1136c913a2e68c
김동환 <donghwan@gimdonghwan-ui-MacBookAir.local>
2025-10-05 03:36:39 +0900

    React 코드 업로드

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -1,13 +1,13 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MainPage from './MainPage';
-import SdiPage from './SdiPage'; 
+import Sdi_Pdi_Page from './Sdi_Pdi_Page'; 
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<MainPage />} />
-        <Route path="/sdi" element={<SdiPage />} />
+        <Route path="/sdi" element={<Sdi_Pdi_Page />} />
       </Routes>
     </BrowserRouter>
   );
